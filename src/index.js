9bfe38774dbd904c1c8deaaa869de67a58eca676
James Henry <james@henry.sc>
2016-02-20 22:01:26 +0000

    test(Test): Added mocha and chai for testing

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function generate() {
+
+    return {}
+
+}
+
+module.exports = generate
---
+++ src/index.spec.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect
+const generateTSInterface = require('./index')
+
+describe( 'mongoose-schema-to-typescript-interface', () => {
+
+    it( 'should work', () => {
+
+        expect( true ).to.be.true
+
+    })
+
+})
